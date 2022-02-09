1b9d8a6665af14cdeb30c41f2b8855a1bbbcd401
trizhaaaaa <trizhamaglangit@gmail.com>
2022-02-10 04:03:10 +0800

    updated contents on contact-page; table-contact info (wd, nr)

--- escord-v1.0/src/components/Contact.vue
+++ escord-v1.0/src/components/Contact.vue
@@ -1,14 +1,40 @@
 <template>
   <div id="contact">
     <h1 class="landing-page-title">contact us</h1>
+
     <p>
       For inquiries about ESCORD or other provided services, you may refer to
       the provided contact information below to get in touch with the team.
     </p>
 
-    <p class="contactInfo">E-MAIL ADDRESS: ucc-escord.bscs2019@gmail.com</p>
-    <p class="contactInfo">CONTACT NUMBERS: 0923 435 2346/ 0978 356 8729</p>
-    <p class="contactInfo">LANDLINE: (02) 9332 4355</p>
+    <center>
+      <table>
+        <tr>
+          <td class="title">E-MAIL ADDRESS:</td>
+          <td>ucc-escord.bscs2019@gmail.com</td>
+        </tr>
+
+        <tr>
+          <td class="title">CONTACT NUMBERS:</td>
+          <td>0923 435 2346/ 0978 356 8729</td>
+        </tr>
+
+        <tr>
+          <td></td>
+          <td class="sched">10AM-4PM | Mon-Fri</td>
+        </tr>
+
+        <tr>
+          <td class="title">LANDLINE:</td>
+          <td>(02) 9332 4355</td>
+        </tr>
+
+        <tr>
+          <td></td>
+          <td class="sched">10AM-4PM | Mon-Fri</td>
+        </tr>
+      </table>
+    </center>
   </div>
 </template>
 
@@ -21,8 +47,20 @@ export default {
 </script>
 
 <style scoped>
-p.contactInfo {
+tr {
   font-family: "Cuprum", sans-serif;
   font-size: 20px;
+}
+td {
+  padding: 5px;
+  vertical-align: middle;
+}
+td.title {
+  font-weight: bold;
+  text-align: right;
+}
+td.sched {
+  font-size: 16px;
+  text-shadow: 0px 0px 3px #ff9807;
 }
 </style>
