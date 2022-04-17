7fa40dd7b2ee2f677f022213873950be1dc41247
Jackdaisuki04@gmail.com <jackdaisuki04@gmail.com>
2022-04-17 10:12:39 +0800

    Trial in API v1

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,14 +21,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
  Route::group(['middleware' => 'auth:sanctum'], function() {
   
     Route::post('/logout','AdminController@logout');
+   
 
   });
 
     Route::post('/gradesheetstudent','GradesheetController@addgradestudent');
     Route::post('/gradesheetinfo','GradesheetController@addgradesheetinfo');
-    Route::get('/showgs','GradesheetController@showgradesheet');
+    
     Route::get('/showgs/{gradesheetid}','GradesheetController@showgsbyid');
-
+    Route::get('/showgs','GradesheetController@showgradesheet');
     //this is for multirows
     Route::post('/addgs','GradesheetController@addgs');
 
